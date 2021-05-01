d1c1273259860730b0b9a394f46bf98e93bc1ac3
Yuldashev222 <oybekyuldashov54@gmail.com>
2021-05-01 09:28:05 +0500

    hammasi

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+var playBtn = document.querySelector(".play-btn");
+var embed = document.querySelector(".embed");
+
+playVideo.addEventListener("click", function() {
+	embed.classList.toggle('embed--active');
+	playBtn.classList.toggle('play-btn--active');
+});
